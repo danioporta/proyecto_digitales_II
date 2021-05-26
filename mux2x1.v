264ae8057976f614dcc14d8cfd43eebf80548e24
rokandrecr <31490830+rokandrecr@users.noreply.github.com>
2021-05-25 19:08:49 -0600

    Add files via upload

--- mux2x1.v
+++ mux2x1.v
@@ -1,7 +1,8 @@
-module Mux2x1 (output reg Salida,
-            output validSalida,
-            input  Entrada0,
-            input  Entrada1,
+module Mux2x1 (output reg [7:0] Salida,
+            output reg validSalida,
+            
+	    input [7:0] Entrada0,
+            input [7:0] Entrada1,
             input validEntrada0,
             input validEntrada1,
             input selector, 
@@ -9,7 +10,7 @@ module Mux2x1 (output reg Salida,
             input reset);
             
             always@(posedge clk) begin
-                if (reset_L==1) //Si el reset es 1 el flop puede tomar el valor de alguna de las dos entradas
+                if (reset==1) //Si el reset es 1 el flop puede tomar el valor de alguna de las dos entradas
                     if(selector==0)
                         Salida<=Entrada0; // Segun el valor del selector este dato irá a la salida del flop
                     else
